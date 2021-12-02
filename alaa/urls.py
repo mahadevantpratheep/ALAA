56f2ef83d5a465ea86348bfb42258907659f358e
MAHADEVAN T PRATHEEP <39547520+mahadevantpratheep@users.noreply.github.com>
2021-12-02 12:05:31 +0530

    Update urls.py

--- alaa/urls.py
+++ alaa/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('', include('forms.urls')),
-    path('admin/', admin.site.urls),
+    #path('', include('forms.urls')),
+    #path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
